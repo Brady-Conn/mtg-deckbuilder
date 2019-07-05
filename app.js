fd6d00d11d442117cb99406ac7a08bca73e1c360
jbc5099 <jbc5099@gmail.com>
2019-07-04 19:35:10 -0600

    removing mtgsdk files from repo

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var request = new XMLHttpRequest()
+let request = new XMLHttpRequest()
 
-request.open('GET', 'https://api.magicthegathering.io/v1/cards', true)
+request.open('GET', 'https://api.magicthegathering.io/v1/sets', true)
 request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
+  let data = JSON.parse(this.response)
 
   if (request.status >= 200 && request.status < 400) {
-    data.cards.forEach(card => {
-      console.log(card.name)
-    })
+    // data.cards.forEach(card => {
+    //   console.log(card.name)
+    // })
+    console.log(data)
   } else {
     console.log('error')
   }

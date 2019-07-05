cdf8ad0e17a181ceac18740dc52180591b38afe2
jbc5099 <jbc5099@gmail.com>
2019-07-05 14:21:13 -0600

    a few more style changes, testing differnt api calls, added jquery

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let request = new XMLHttpRequest()
 
-request.open('GET', 'https://api.magicthegathering.io/v1/sets', true)
+request.open('GET', 'https://api.magicthegathering.io/v1/cards?set=MH1', true)
 request.onload = function() {
   let data = JSON.parse(this.response)
 
   if (request.status >= 200 && request.status < 400) {
-    // data.cards.forEach(card => {
-    //   console.log(card.name)
-    // })
-    console.log(data)
+    data.cards.forEach(card => {
+      
+    })
+    //console.log(data)
   } else {
     console.log('error')
   }

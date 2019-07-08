88a95378b78e5c24286114778486bcd5d23aad74
jbc5099 <jbc5099@gmail.com>
2019-07-07 21:49:25 -0600

    re wrote api call with fetch as it was taking a long time to load, not much better, thinking about caching called data

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-let request = new XMLHttpRequest()
-
-request.open('GET', 'https://api.magicthegathering.io/v1/cards?set=MH1', true)
-request.onload = function() {
-  let data = JSON.parse(this.response)
-
-  if (request.status >= 200 && request.status < 400) {
-     
+fetch('https://api.magicthegathering.io/v1/cards?set=MH1')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
     data.cards.forEach(card => {
       let $card = $('<div class="card"></div>')
       let image = card.imageUrl
       $card.css('background-image', 'url(' + image + ')')
       $('.card-display').append($card)
     })
-  } else {
-    console.log('error')
-  }
-}
-
-request.send()
+  });

aa0e79ee0fb17008957c5fe37dd7d0af0e99bc9b
jbc5099 <jbc5099@gmail.com>
2019-07-08 21:30:55 -0600

    added further styling and started working on nav framework in app.js

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 if (localStorage.getItem('mh1') === null){
-  fetch('https://api.magicthegathering.io/v1/cards?page=2&set=MH1')
+  fetch('https://api.magicthegathering.io/v1/cards?page=1&set=MH1')
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
       localStorage.setItem('mh1', JSON.stringify(data))
-      data.cards.forEach(card => {
-        let $card = $('<div class="card"></div>')
-        let image = card.imageUrl
-        $card.css('background-image', 'url(' + image + ')')
-        $('.card-display').append($card)
-      })
     });
   }
 
@@ -34,3 +28,15 @@ if(localStorage.getItem('mh3') === null){
     })
 }
 
+$( document ).ready(function() {
+  if ($('.page-count').text() === 'Page:'){
+    $('.page-count').text('Page: 1 of 3');
+    let mh1 = JSON.parse(localStorage.getItem('mh1'))
+    mh1.cards.forEach(card => {
+      let $card = $('<div class="card"></div>')
+      let image = card.imageUrl
+      $card.css('background-image', 'url(' + image + ')')
+      $('.card-display').append($card)
+    })
+  }
+});

1c06679860bc1d79a9f12d74dccbcc4123c546eb
jbc5099 <jbc5099@gmail.com>
2019-07-08 20:33:34 -0600

    added additional fetches for the rest of the cards I was looking for, storing them in localStorage, will only fetch if not already in local storage

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-fetch('https://api.magicthegathering.io/v1/cards?set=MH1')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    data.cards.forEach(card => {
-      let $card = $('<div class="card"></div>')
-      let image = card.imageUrl
-      $card.css('background-image', 'url(' + image + ')')
-      $('.card-display').append($card)
+if (localStorage.getItem('mh1') === null){
+  fetch('https://api.magicthegathering.io/v1/cards?page=2&set=MH1')
+    .then(function(response) {
+      return response.json();
     })
-  });
+    .then(function(data) {
+      localStorage.setItem('mh1', JSON.stringify(data))
+      data.cards.forEach(card => {
+        let $card = $('<div class="card"></div>')
+        let image = card.imageUrl
+        $card.css('background-image', 'url(' + image + ')')
+        $('.card-display').append($card)
+      })
+    });
+  }
+
+if (localStorage.getItem('mh2') === null){
+  fetch('https://api.magicthegathering.io/v1/cards?page=2&set=MH1')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      localStorage.setItem('mh2', JSON.stringify(data))
+    })
+  }
+
+if(localStorage.getItem('mh3') === null){
+  fetch('https://api.magicthegathering.io/v1/cards?page=3&set=MH1')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      localStorage.setItem('mh3', JSON.stringify(data))
+    })
+}
+
